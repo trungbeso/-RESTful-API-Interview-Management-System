8fe0e7432a3e049fe9a1c2623bf33555eb9f182a
trungnq555 <trungnq555gmail.com>
2025-02-03 21:14:51 +0700

    feat(project): Security Configuration

---
+++ src/main/java/com/interviewmanagementsystem/configuration/JWTFilter.java
@@ -0,0 +1,45 @@
+package com.interviewmanagementsystem.configuration;
+
+import com.ninja_in_pyjamas.services.auth.token.ITokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JWTFilter extends GenericFilterBean {
+
+	private final ITokenService tokenService;
+
+	public JWTFilter(ITokenService tokenService) {
+		this.tokenService = tokenService;
+	}
+
+	@Override
+	public void doFilter(
+		  ServletRequest servletRequest,
+		  ServletResponse servletResponse,
+		  FilterChain filterChain) throws IOException, ServletException {
+
+		HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+		String bearerToken = httpServletRequest.getHeader("Authorization");
+		String jwtToken = null;
+
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			jwtToken = bearerToken.substring(7);
+		}
+
+		Authentication authentication = tokenService.getAuthentication(jwtToken);
+
+		if (authentication != null) {
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+}
---
+++ src/main/java/com/interviewmanagementsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,88 @@
+package com.interviewmanagementsystem.configuration;
+
+import com.ninja_in_pyjamas.services.auth.token.ITokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+	private final ITokenService tokenService;
+
+	public SecurityConfiguration(ITokenService tokenService) {
+		this.tokenService = tokenService;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		// Allow 4200 and 3000 ports for development
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+		config.addAllowedHeader("*"); // X-Requested-With, Content-Type, Authorization, Origin, Accept
+		config.addAllowedMethod("*"); // GET, POST, PUT, DELETE, PATCH, OPTIONS
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost:5173");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+		  .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
+      .addFilterBefore(new JWTFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/api/auth/**").permitAll()
+					.requestMatchers("/forgotPassword", "/forgotPassword/**").permitAll()
+				  .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
+				  .requestMatchers("/api/employees/**").permitAll()
+				  .requestMatchers("/api/candidates/**").authenticated()
+				  .requestMatchers("/api/offers/**").permitAll()
+				  .requestMatchers("/api/interviews/**").authenticated()
+				  .requestMatchers("/api/jobs/**").permitAll()
+					.requestMatchers("/api/roles/**").permitAll()
+					.requestMatchers("/api/departments/**").permitAll()
+					.requestMatchers("/api/skills/**").permitAll()
+					.requestMatchers("/api/benefits/**").permitAll()
+					.requestMatchers("/api/imports/**").permitAll()
+					.requestMatchers("/api/forgotPassword/**").permitAll()
+				  .anyRequest().authenticated()
+			  );
+		return http.build();
+	}
+
+}
