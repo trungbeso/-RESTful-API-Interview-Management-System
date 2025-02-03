2e516b517ed78c47d11d17f5ffa712aee418e9d5
trungnq555 <trungnq555gmail.com>
2025-02-03 21:02:17 +0700

    feat(project): Create base project

---
+++ src/main/java/com/interviewmanagementsystem/InterviewManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.interviewmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewmanagementsystem/InterviewManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
